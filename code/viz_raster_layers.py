1423235580df74450dd0d90282632cdcc73af8dc
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2023-04-17 15:45:24 +0200

    Also visualizing granular data.

--- code/viz_raster_layers.py
+++ code/viz_raster_layers.py
@@ -1,6 +1,7 @@
 # %% 
 import os
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from osgeo import gdal
 os.getcwd()
@@ -72,4 +73,71 @@ plt.savefig('../figures/population_layers.png', dpi = 200)
 plt.show()
 
 
+# %% Now we will plot the estimated wealth index and the RWI
+SA_IWI = pd.read_csv("../data/South Africa_estimated_wealth_index.csv").rename({"estimated_IWI" : "IWI"}, axis=1)
+SA_IWI = SA_IWI[SA_IWI.lon.between(minlon, maxlon) & SA_IWI.lat.between(minlat, maxlat)]
+SA_RWI = pd.read_csv("../data/South Africa_RWI.csv").rename({"longitude" : "lon", "latitude" : "lat", "rwi" : "RWI"}, axis=1)
+SA_RWI = SA_RWI[SA_RWI.lon.between(minlon, maxlon) & SA_RWI.lat.between(minlat, maxlat)]
+
+# %%
+import geopandas as gpd
+south_africa = gpd.read_file("../data/spatial_tax_panel/Spatial_Tax_Panel_v3/Shapefiles/MDB_Local_Municipal_Boundary_2018/MDB_Local_Municipal_Boundary_2018.shp") \
+                .clip_by_rect(xmin = minlon, ymin = minlat, xmax = maxlon, ymax = maxlat) 
+# %% 
+# IWI
+fig, ax = plt.subplots(figsize = (12, 12))
+# fig.suptitle('International Wealth Index', fontsize=16)
+ax.set_aspect('equal')
+ax.margins(0) 
+ax.set_axis_off() # Only Bbox: ax.set_frame_on(False)
+south_africa.plot(ax = ax, linewidth = 0.05, color='none', edgecolor='lightgrey') # black
+plt.scatter(x = SA_IWI.lon, y = SA_IWI.lat, c = SA_IWI.IWI, marker=".", 
+            s = 2, cmap="turbo", edgecolors='none', vmin=0, vmax=100)
+cb = plt.colorbar(shrink = 0.5, pad = 0.02)
+cb.outline.set_color(None)
+cb.ax.set_title('IWI')
+plt.savefig("../figures/SA_IWI.pdf", 
+            dpi = 300, format="pdf", bbox_inches="tight")
+plt.savefig("../figures/SA_IWI.png", 
+            dpi = 300, format="png", bbox_inches="tight")
+plt.show()
+# %% 
+# RWI
+fig, ax = plt.subplots(figsize = (12, 12))
+ax.set_aspect('equal')
+ax.margins(0) 
+ax.set_axis_off() # Only Bbox: ax.set_frame_on(False)
+south_africa.plot(ax = ax, linewidth = 0.05, color='none', edgecolor='lightgrey') # black
+plt.scatter(x = SA_RWI.lon, y = SA_RWI.lat, c = SA_RWI.RWI, marker=".", 
+            s = 2, cmap="turbo", edgecolors='none') # , vmin=0, vmax=100
+cb = plt.colorbar(shrink = 0.5, pad = 0.02)
+cb.outline.set_color(None)
+cb.ax.set_title('RWI')
+plt.savefig("../figures/SA_RWI.pdf", 
+            dpi = 300, format="pdf", bbox_inches="tight")
+plt.savefig("../figures/SA_RWI.png", 
+            dpi = 300, format="png", bbox_inches="tight")
+plt.show()
+# %%
+## NL20
+data_path = "../data/south_africa_viirs_dnb_nightlights_v1_vcmslcfg_annual_median_composite"
+gdal.Translate('/vsimem/clip.tif', os.path.join(data_path,  '2020.tif'), projWin=bbox)
+img = gdal.Open('/vsimem/clip.tif')
+img_arr = np.log10(img.GetRasterBand(1).ReadAsArray())
+# replace infinite values with nan
+img_arr[img_arr == -np.inf] = np.nan
+del img
+# %%
+fig, ax = plt.subplots(figsize = (12, 12))
+ax.set_aspect('equal')
+ax.margins(0) 
+ax.set_axis_off() # Only Bbox: ax.set_frame_on(False)
+plt.imshow(img_arr, cmap="turbo", interpolation='nearest')
+# south_africa.plot(ax = ax, linewidth = 0.05, color='none', edgecolor='black') # black
+cb = plt.colorbar(shrink = 0.5, pad = 0.02)
+cb.outline.set_color(None)
+cb.ax.set_title('NL20')
+plt.savefig("../figures/SA_NL20.png", 
+            dpi = 200, format="png", bbox_inches="tight")
+plt.show()
 # %%
