6473f6fb21ff91e2d409cc03fb91589844724a17
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2023-04-20 16:20:42 +0200

    Adding title.

--- code/download_SA_pop.R
+++ code/download_SA_pop.R
@@ -1,4 +1,7 @@
-
+########################################################
+# Download WorldPop UN-Adjusted 1km2 Population for SA
+# Source: https://hub.worldpop.org/geodata/listing?id=75
+########################################################
 
 pop_year <- function(y) paste0("https://data.worldpop.org/GIS/Population/Global_2000_2020_1km_UNadj/", y, "/ZAF/zaf_ppp_", y, "_1km_Aggregated_UNadj.tif")
 
